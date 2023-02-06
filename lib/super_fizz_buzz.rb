b398316ca881ab148058ba498bbfac9b10cb7bb7
Branden Ge <86636108+brandenge@users.noreply.github.com>
2023-02-06 11:40:35 -0600

    Add SuperFizzBuzz iteration 1

--- lib/super_fizz_buzz.rb
+++ lib/super_fizz_buzz.rb
@@ -1,2 +1,8 @@
-# Iterate through the numbers 1 to 1000
-# For each one, print the output according to the rules of Super Fizz Buzz
+super_fizz = { 'Super' => 7, 'Fizz' => 3, 'Buzz' => 5 }
+(1..1000).each do |num|
+  result = ''
+  super_fizz.each do |key, value|
+    result += key if (num % value).zero?
+  end
+  puts result.empty? ? num : result
+end
